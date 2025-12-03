dfef302c3015e00a321df35b694a2bf98594e654
OldDzen <olddzen@gmail.com>
2025-12-03 16:30:01 +0200

    Change from work laptop

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
        System.out.println("Hello World!");
        System.out.println("Hello new mac!");
-       System.out.println("Hello home mac!");
+       System.out.println("Hello work mac!");
 
     }
 }
