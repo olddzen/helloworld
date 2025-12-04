89fcd90a5916e2431d724062dda0ba1b366240b5
OldDzen <olddzen@gmail.com>
2025-12-04 23:21:30 +0200

    1 Lession with video

--- src/main/java/DogCreator.java
+++ src/main/java/DogCreator.java
@@ -3,11 +3,15 @@ public class DogCreator {
     public static void main(String[] args) {
         Dog barsik = new Dog();
         Dog bobik = new Dog();
+        Dog marko = new Dog();
 
         barsik.name = "barsik";
         bobik.name = "bobik";
+        marko.name = "marko";
+
 
         barsik.bark();
         bobik.bark();
+        marko.bark();
     }
 }
